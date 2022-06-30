79200a5b2c5ca098732d3635edb65f772a0d6031
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-06-30 15:20:00 -0700

    Alter to connect with Twitter and return a response

--- tweettweet.py
+++ tweettweet.py
@@ -10,12 +10,14 @@ consumer_key = keys[0]
 consumer_secret = keys[1]
 access_token = keys[2]
 access_token_secret = keys[3]
+bearer_token = keys[4]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+client = tweepy.Client(
+    bearer_token, consumer_key, consumer_secret, access_token, access_token_secret
+)
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
+public_tweets = client.get_home_timeline()
+for tweet in public_tweets.data:
     print(tweet.text)
