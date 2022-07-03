4d7b2fb5b608ed282adaf0977b0c9a8cb780549b
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-03 14:07:32 -0700

    Add the ability to loop through my followers and follow them back

--- tweettweet.py
+++ tweettweet.py
@@ -16,9 +16,18 @@ client = tweepy.Client(
     bearer_token, consumer_key, consumer_secret, access_token, access_token_secret
 )
 
+# Get list of tweets on my home screen
 # public_tweets = client.get_home_timeline()
 # for tweet in public_tweets.data:
 #     print(tweet.text)
 
-user = client.get_me()
-print(user.data.name)
+# Print info on myself
+# user = client.get_me()
+# print(user.data.name)
+
+# Get a list of my followers and follow them back
+# user = client.get_me()
+# print(user.data.id) # my user id
+# for follower in client.get_users_followers(id=user.data.id).data:
+#     print(follower)
+#     client.follow_user(target_user_id=follower.id)
